02e004b3c715686e17520f0a9cdb1288cec73bb8
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-10-17 15:44:03 +0200

    chore(scripts): Also prebuild x86 on Windows and Linux
    Change-Type: patch

--- scripts/prebuild-publish.bat
+++ scripts/prebuild-publish.bat
@@ -2,6 +2,7 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    npm run prebuild -- -u %GITHUB_TOKEN%
+    npm run prebuild -- --arch x64 -u %GITHUB_TOKEN%
+    npm run prebuild -- --arch x86 -u %GITHUB_TOKEN%
   )
 )
--- scripts/prebuild-publish.sh
+++ scripts/prebuild-publish.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+PLATFORM=$(node -p process.platform)
+
 if [[ $GITHUB_TOKEN ]]; then
-  npm run prebuild -- -u "$GITHUB_TOKEN"
+  npm run prebuild -- --arch x64 -u "$GITHUB_TOKEN"
+  if [[ $PLATFORM == "linux" ]]; then
+    npm run prebuild -- --arch x86 -u "$GITHUB_TOKEN"
+  fi
 fi

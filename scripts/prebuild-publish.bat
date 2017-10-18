6cc1cf9a7a9b85a956769708486edcc73604fa07
Jonas Hermsmeier <jhermsmeier@gmail.com>
2017-10-18 21:14:00 +0200

    chore(scripts): Run CI on x86 as well
    As previous efforts have shown, the `--arch` flags on `prebuild`
    didn't do the expected, and this now runs CI builds on x86 as well.
    
    Change-Type: patch

--- scripts/prebuild-publish.bat
+++ scripts/prebuild-publish.bat
@@ -2,7 +2,6 @@
 
 if %APPVEYOR_REPO_BRANCH% == master (
   if %GITHUB_TOKEN% neq "" (
-    npm run prebuild -- --arch x64 -u %GITHUB_TOKEN%
-    npm run prebuild -- --arch x86 -u %GITHUB_TOKEN%
+    npm run prebuild -- -u %GITHUB_TOKEN%
   )
 )
--- scripts/prebuild-publish.sh
+++ scripts/prebuild-publish.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-PLATFORM=$(node -p process.platform)
-
 if [[ $GITHUB_TOKEN ]]; then
-  npm run prebuild -- --arch x64 -u "$GITHUB_TOKEN"
-  if [[ $PLATFORM == "linux" ]]; then
-    npm run prebuild -- --arch x86 -u "$GITHUB_TOKEN"
-  fi
+  npm run prebuild -- -u "$GITHUB_TOKEN"
 fi
